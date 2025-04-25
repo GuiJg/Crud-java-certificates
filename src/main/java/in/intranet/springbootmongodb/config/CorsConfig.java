f79c9d714f053bf4d959178ef09d81b65408f145
José Guilherme <j.gu1lh3rm325@gmail.com>
2025-04-25 13:59:15 -0300

    feat: properties env prod added cors

--- src/main/java/in/intranet/springbootmongodb/config/CorsConfig.java
+++ src/main/java/in/intranet/springbootmongodb/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package in.intranet.springbootmongodb.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
@@ -7,16 +8,16 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 public class CorsConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:4200",
-                                "https://intranet.absolutacontabilidade.com.br"
-                        )
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
